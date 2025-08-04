f2479d92967ed2b793d69e372df004438da1ccf6
Dean Amar <Dean.Amar@ibm.com>
2025-08-04 14:44:19 +0300

    * Updated YugaCluster yugabyte's tool utilization from yugabyted to a seperated node creation - masters and tablets.
    Signed-off-by: Dean Amar <Dean.Amar@ibm.com>

--- integration/runner/cluster_controller.go
+++ integration/runner/cluster_controller.go
@@ -7,7 +7,6 @@ SPDX-License-Identifier: Apache-2.0
 package runner
 
 import (
-	"context"
 	"testing"
 	"time"
 
@@ -24,11 +23,10 @@ type DBClusterController struct {
 }
 
 const (
-	//nolint:revive // LeaderNode and FollowerNode represent db nodes role.
-	LeaderNode   = "leader"
-	FollowerNode = "follower"
-	TabletNode   = "tablet"
+	//nolint:revive // MasterNode, FollowerNode and TabletNode represent db nodes role.
 	MasterNode   = "master"
+	TabletNode   = "tablet"
+	FollowerNode = "follower"
 
 	linuxOS = "linux"
 )
@@ -53,20 +51,6 @@ func (cc *DBClusterController) StopAndRemoveNodeWithRole(t *testing.T, role stri
 	}
 }
 
-// StopAndRemoveNodeWithName stops and removes a node given a name.
-func (cc *DBClusterController) StopAndRemoveNodeWithName(t *testing.T, name string) {
-	t.Helper()
-	require.NotEmpty(t, cc.nodes, "trying to remove nodes of an empty cluster.")
-	for idx, node := range cc.nodes {
-		if node.Name == name {
-			t.Logf("found ")
-			node.StopAndRemoveContainer(t)
-			cc.nodes = append(cc.nodes[:idx], cc.nodes[idx+1:]...)
-			return
-		}
-	}
-}
-
 // GetClusterSize returns the number of active nodes in the cluster.
 func (cc *DBClusterController) GetClusterSize() int {
 	return len(cc.nodes)
@@ -80,31 +64,6 @@ func (cc *DBClusterController) GetNodesContainerID(t *testing.T) []string {
 		containersIDs = append(containersIDs, node.ContainerID())
 	}
 	return containersIDs
-}
-
-func (cc *DBClusterController) getLeaderHost(ctx context.Context, t *testing.T) string {
-	t.Helper()
-
-	require.NotEmpty(t, cc.nodes, "no nodes available in cluster")
-
-	for _, node := range cc.nodes {
-		if node.Role == LeaderNode {
-			return node.GetContainerConnectionDetails(ctx, t).GetHost()
-		}
-	}
-
-	t.Fatal("no leader node found in cluster")
-	return "" // unreachable, but required for compiler
-}
-
-func (cc *DBClusterController) getNodesConnections(ctx context.Context, t *testing.T) *dbtest.Connection {
-	t.Helper()
-	endpoints := make([]*connection.Endpoint, cc.GetClusterSize())
-	for i, node := range cc.nodes {
-		endpoints[i] = node.GetContainerConnectionDetails(ctx, t)
-	}
-
-	return dbtest.NewConnection(endpoints...)
 }
 
 func (cc *DBClusterController) stopAndRemoveCluster(t *testing.T) {
