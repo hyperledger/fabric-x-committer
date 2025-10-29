4f2ee866160c9c9d5dc814f410effcbeaff91886
Effi-S <effi.szt@gmail.com>
2025-10-29 14:38:39 +0200

    Updated changes to reflect upstream repo
    Signed-off-by: Effi-S <effi.szt@gmail.com>

--- utils/serialization/envelope_wrapper_test.go
+++ utils/serialization/envelope_wrapper_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/hyperledger/fabric-protos-go-apiv2/common"
-	"github.com/hyperledger/fabric/protoutil"
+	"github.com/hyperledger/fabric-x-common/protoutil"
 
 	"github.com/hyperledger/fabric-x-committer/utils/serialization"
 	"github.com/hyperledger/fabric-x-committer/utils/test"
@@ -19,6 +19,7 @@ import (
 
 // TestUnwrapEnvelopeBadInput tests UnwrapEnvelope function with invalid inputs.
 func TestUnwrapEnvelopeBadInput(t *testing.T) {
+	t.Parallel()
 	t.Run("Not an envelope", func(t *testing.T) {
 		t.Parallel()
 		_, _, err := serialization.UnwrapEnvelope([]byte("invalid input"))
@@ -43,11 +44,12 @@ func TestUnwrapEnvelopeBadInput(t *testing.T) {
 			Header: nil,
 			Data:   []byte("some data"),
 		}
-		payloadBytes := protoutil.MarshalOrPanic(payload)
 
+		payloadBytes := protoutil.MarshalOrPanic(payload)
 		envelope := &common.Envelope{
 			Payload: payloadBytes,
 		}
+
 		envelopeBytes := protoutil.MarshalOrPanic(envelope)
 
 		_, _, err := serialization.UnwrapEnvelope(envelopeBytes)
@@ -78,19 +80,31 @@ func TestUnwrapEnvelopeBadInput(t *testing.T) {
 // TestUnwrapEnvelopeGoodInput Tests properly wrapped envelope is unwrapped correctly.
 func TestUnwrapEnvelopeGoodInput(t *testing.T) {
 	t.Parallel()
-	// -1- Check unwrap envelope has no error
 	originalPayload := []byte("test payload")
+
 	originalChannelHeader := &common.ChannelHeader{
 		ChannelId: "test-channel",
 	}
+
 	originalHeader := &common.Header{
 		ChannelHeader: protoutil.MarshalOrPanic(originalChannelHeader),
 	}
-	wrappedEnvelope := serialization.WrapEnvelope(originalPayload, originalHeader)
+
+	// Wrap
+	wrappedEnvelope := protoutil.MarshalOrPanic(&common.Envelope{
+		Payload: protoutil.MarshalOrPanic(&common.Payload{
+			Header: originalHeader,
+			Data:   originalPayload,
+		}),
+	})
+
+	// Unwrap
 	payload, channelHeader, err := serialization.UnwrapEnvelope(wrappedEnvelope)
 
-	// -2- Check we get the correct Payload & Header
+	// -Check 1- Check unwrap envelope has no error
 	require.NoError(t, err)
+
+	// -Check 2- Check we get the correct Payload & Header
 	require.Equal(t, originalPayload, payload)
 	test.RequireProtoEqual(t, originalChannelHeader, channelHeader)
 }
