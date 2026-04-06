07c867cae8a68c05211b4cb62467f271fd7512d2
Senthil Nathan N <cendhu@gmail.com>
2026-04-06 18:03:27 +0530

    [test] Add make ci-local to run full CI flow locally (#518)
    #### Type of change
    
    - Improvement (improvement to code, performance, etc)
    - Test update
     
    #### Description
    
      Add ci-local.sh that manages postgres and yugabyte containers,
      runs all CI test steps, and cleans up on exit. Replace the native
      yugabyte install scripts with Docker-based versions. Remove the
      stale ci-flow.sh. Fix flaky loadgen abort assertion.
    
    #### Related issues
    
      - resolves #517 
      - resolves #519
    
    Signed-off-by: Senthilnathan <cendhu@gmail.com>

--- scripts/get-and-start-yuga.sh
+++ scripts/get-and-start-yuga.sh
@@ -5,42 +5,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-YUGA_DIR=${YUGA_DIR:="$HOME/bin/yugabyte"}
+YUGA_IMAGE="yugabytedb/yugabyte:2025.2.0.1-b1"
+YUGA_CONTAINER="sc_test_yugabyte"
 
-if [ ! -d "$YUGA_DIR" ]; then
-  echo "Downloading and extracting YugabyteDB"
-  mkdir -p "$YUGA_DIR"
-  curl https://downloads.yugabyte.com/releases/2025.2.0.1/yugabyte-2025.2.0.1-b1-linux-x86_64.tar.gz |
-    tar --strip=1 -xz -C "$YUGA_DIR"
+echo "Downloading YugabyteDB"
+docker pull "$YUGA_IMAGE"
 
-  cd "$YUGA_DIR" || exit 1
-
-  echo "Installing YugabyteDB"
-  bin/post_install.sh
-fi
-
-cd "$YUGA_DIR" || exit 1
-
-echo "Unlimited open files and enabling transparent huge pages..."
-ulimit -n 100000
-echo always | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
-echo always | sudo tee /sys/kernel/mm/transparent_hugepage/defrag
-
-DATA_DIR=$(mktemp -d -t "yuga.XXXX")
-echo "Using temporary data dir: $DATA_DIR"
-
-echo "Running YugabyteDB"
-alias python='${PYTHON_CMD:-python}'
-${PYTHON_CMD:-python} -m pip install setuptools
-bin/yugabyted start \
-  --advertise_address 0.0.0.0 \
-  --callhome false \
-  --fault_tolerance none \
-  --background true \
-  --ui false \
-  --base_dir "$DATA_DIR" \
-  --insecure \
-  --tserver_flags "ysql_max_connections=500,tablet_replicas_per_gib_limit=4000,yb_num_shards_per_tserver=1,minloglevel=3,yb_enable_read_committed_isolation=true"
+echo "Running YugabyteDB container"
+docker run --name "$YUGA_CONTAINER" \
+  -p 5433:5433 \
+  -d "$YUGA_IMAGE" \
+  bin/yugabyted start \
+    --advertise_address 0.0.0.0 \
+    --callhome false \
+    --fault_tolerance none \
+    --background false \
+    --ui false \
+    --insecure \
+    --tserver_flags "pgsql_proxy_bind_address=0.0.0.0,ysql_max_connections=500,tablet_replicas_per_gib_limit=4000,yb_num_shards_per_tserver=1,minloglevel=3,yb_enable_read_committed_isolation=true"
 #  By default, 1 GB of memory reserved for a YB-Tserver can support up to 1497
 #  tablets. When tests are run in parallel, this limit is sometimes reached in
 #  environments with low resource allocation, causing the test to fail. To handle
