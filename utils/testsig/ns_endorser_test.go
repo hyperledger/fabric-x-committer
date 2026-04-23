af10935cbbe5d7880c4b0ef170334dbbaa13bb2d
Liran Funaro <liran.funaro@gmail.com>
2026-04-23 12:21:19 +0300

    Moved testcrypto package to fabric-x-common
    Signed-off-by: Liran Funaro <liran.funaro@gmail.com>

--- utils/testsig/ns_endorser_test.go
+++ utils/testsig/ns_endorser_test.go
@@ -13,11 +13,11 @@ import (
 	"testing"
 
 	"github.com/hyperledger/fabric-x-common/api/applicationpb"
+	"github.com/hyperledger/fabric-x-common/utils/testcrypto"
 	"github.com/stretchr/testify/require"
 
 	"github.com/hyperledger/fabric-x-committer/utils/signature"
 	"github.com/hyperledger/fabric-x-committer/utils/test"
-	"github.com/hyperledger/fabric-x-committer/utils/testcrypto"
 )
 
 const testTxID = "test-tx-id"
