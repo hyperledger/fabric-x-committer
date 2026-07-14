6906994bfc10804c423a15d104eebcc9055d1009
Dean Amar <Dean.Amar@ibm.com>
2026-07-14 16:42:54 +0300

    * prevent circular dependencies.
    Signed-off-by: Dean Amar <Dean.Amar@ibm.com>

--- service/coordinator/dependencygraph/manager_test.go
+++ service/coordinator/dependencygraph/manager_test.go
@@ -19,11 +19,12 @@ import (
 
 	"github.com/hyperledger/fabric-x-committer/api/servicepb"
 	"github.com/hyperledger/fabric-x-committer/loadgen/workload"
-	"github.com/hyperledger/fabric-x-committer/service/coordinator"
 	"github.com/hyperledger/fabric-x-committer/utils/channel"
 	"github.com/hyperledger/fabric-x-committer/utils/monitoring"
 	"github.com/hyperledger/fabric-x-committer/utils/test"
 )
+
+const DefaultQueueMonitorSamplingTime = 100 * time.Millisecond
 
 type batchWithLatency struct {
 	batch TxNodeBatch
@@ -88,7 +89,7 @@ func BenchmarkDependencyGraph(b *testing.B) {
 								IncomingValidatedTxsNode:  val,
 								NumOfLocalDepConstructors: tc.workers,
 								WaitingTxsLimit:           20_000_000,
-								QueueMonitorSamplingTime:  coordinator.DefaultQueueMonitorSamplingTime,
+								QueueMonitorSamplingTime:  DefaultQueueMonitorSamplingTime,
 								PrometheusMetricsProvider: monitoring.NewProvider(),
 							})
 
@@ -182,7 +183,7 @@ func TestDependencyGraphManager(t *testing.T) {
 				IncomingValidatedTxsNode:  validatedTxs,
 				NumOfLocalDepConstructors: 2,
 				WaitingTxsLimit:           waitingTXsLimit,
-				QueueMonitorSamplingTime:  coordinator.DefaultQueueMonitorSamplingTime,
+				QueueMonitorSamplingTime:  DefaultQueueMonitorSamplingTime,
 				PrometheusMetricsProvider: monitoring.NewProvider(),
 			})
 
